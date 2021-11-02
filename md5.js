d51f176b32d1ccbf1c68ea566ebc991d594f6dc4
xueshengjcx <90943412+xueshengjcx@users.noreply.github.com>
2021-11-02 15:26:10 +0000

    Update md5.js

--- md5.js
+++ md5.js
@@ -219,7 +219,7 @@ function binl2str(bin) {
  * Convert an array of little-endian words to a hex string.
  */
 function binl2hex(binarray) {
-    var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789ABCDEF";
+    var hex_tab = hexcase ? "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var str = "";
     for (var i = 0; i < binarray.length * 4; i++) {
         str += hex_tab.charAt((binarray[i >> 2] >> ((i % 4) * 8 + 4)) & 0xF) +
