2258b3d36e8ee80d2bf38d11498f6a5aea5a6d90
xueshengjcx <90943412+xueshengjcx@users.noreply.github.com>
2022-03-26 22:38:30 +0000

    Add files via upload

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import {GLTFLoader} from './GLTFLoader.js'
+import {GLTFLoader} from 'GLTFLoader.js'
 
 const loader = new GLTFLoader();
 const scene = new THREE.Scene()
